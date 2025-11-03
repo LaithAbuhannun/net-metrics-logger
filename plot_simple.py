82a625cf0587e21f8a8f7477fcdf93ed4217913e
Laith Abuhannun <105604310+LaithAbuhannun@users.noreply.github.com>
2025-11-03 14:43:17 +1100

    Update plot_simple.py

--- plot_simple.py
+++ plot_simple.py
@@ -6,7 +6,6 @@ df = pd.read_csv("net_metrics.csv", parse_dates=["timestamp"]).sort_values("time
 df = df.dropna(subset=["download_mbps","upload_mbps","ping_ms"])
 
 # Display the upload speed and the download speed
-plt.figure()
 plt.plot(df["timestamp"], df["download_mbps"], label="Download (Mbps)")
 plt.plot(df["timestamp"], df["upload_mbps"],   label="Upload (Mbps)")
 plt.title("Throughput over time")
@@ -18,7 +17,8 @@ plt.show()
 
 # Display the latency
 plt.plot(df["timestamp"], df["ping_ms"],   label="Ping (ms)")
-plt.xlabel("Time"); plt.ylabel("Ms")
+plt.xlabel("Time")
+plt.ylabel("Ms")
 plt.legend()
 plt.tight_layout()
 plt.show()
